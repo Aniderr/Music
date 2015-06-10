42f34e6be19625a4c89d325c6af77247c0bfd98b
Niejq <aniderr@163.com>
2015-06-10 13:18:30 +0800

    增加歌曲信息实体bean对象

---
+++ Music/src/com/jackie/music/MusicBean.java
@@ -0,0 +1,112 @@
+/**
+ * 
+ */
+package com.jackie.music;
+
+/**
+ * 歌曲文件信息实体对象
+ * @author Jackie
+ *
+ */
+public class MusicBean {
+
+	//歌曲的id
+	private int _id;
+	
+	//歌曲名称
+	private String tilte ;
+	
+	//歌曲所属专辑
+	private String album;
+	
+	//歌手名称
+	private String artist;
+	
+	//歌曲文件路径
+	private String url;
+	
+	//歌曲文件的名称，这里不是歌曲名
+	private String display_name;
+	
+	//歌曲发行日期
+	private String year;
+	
+	//歌曲的总播放时长
+	private int duration;
+	
+	private int size;
+
+	public int get_id() {
+		return _id;
+	}
+
+	public void set_id(int _id) {
+		this._id = _id;
+	}
+
+	public String getTilte() {
+		return tilte;
+	}
+
+	public void setTilte(String tilte) {
+		this.tilte = tilte;
+	}
+
+	public String getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(String album) {
+		this.album = album;
+	}
+
+	public String getArtist() {
+		return artist;
+	}
+
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getDisplay_name() {
+		return display_name;
+	}
+
+	public void setDisplay_name(String display_name) {
+		this.display_name = display_name;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+}
--- Music/src/com/jackie/music/SongSingleActivity.java
+++ Music/src/com/jackie/music/SongSingleActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.music.R;
 import com.jackie.music.constant.MusicConstant;
@@ -24,119 +25,151 @@ import com.lidroid.xutils.view.annotation.event.OnItemClick;
 
 /**
  * @author jackie
- *
+ * 
  */
 
 @ContentView(R.layout.song_single)
-public class SongSingleActivity extends Activity{
+public class SongSingleActivity extends Activity {
 
-	//音乐文件路径
+	// 音乐文件路径
 	private static final String PATH = "/mnt/sdcard/Music";
-	
-	//音乐文件名数组
-	private List<String> names = new ArrayList<String>();
-	
-	//获取listview控件
+
+	String str;
+	// 音乐文件名数组
+	private List<MusicBean> musicBeans = new ArrayList<MusicBean>();
+
+	// 获取listview控件
 	@ViewInject(R.id.SongSingle)
 	private ListView listView;
-	
+
+	private Cursor cursor;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		ViewUtils.inject(this);
-		
+
 		loadMp3();
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<>(getApplicationContext(), R.layout.song_single_item, 
-				R.id.single_item, names);
-		
+
+		ArrayAdapter<MusicBean> adapter = new ArrayAdapter<MusicBean>(
+				getApplicationContext(), R.layout.song_single_item,
+				R.id.single_item, musicBeans);
+
 		listView.setAdapter(adapter);
-		
+
 	}
-	
-	
+
 	/**
 	 * listview每一项的单击事件，这里要说明一点，使用注解方式创建监听方法的同时也得
 	 */
 	@OnItemClick(R.id.SongSingle)
-	public void itemClick(AdapterView<?> parent, View view, int position,long id){
-		
-		//判断是否存在音乐信息
-		if(names != null){
-			
-			/*Intent intent = new Intent();
-			intent.putExtra("url",  getUrl(names.get(position)));         
-            intent.putExtra("MSG", MusicConstant.PLAY_MSG);  
-            intent.setClass(SongSingleActivity.this, PlayService.class);  
-            startService(intent);       //启动服务  
-*/            
-            Intent intent2 = new Intent();
-            intent2.putExtra("url",  getUrl(names.get(position)));         
-            intent2.putExtra("MSG", MusicConstant.PLAY_MSG);  
-            intent2.setClass(this,PlayerActivity.class);
-            startActivity(intent2);
-            
+	public void itemClick(AdapterView<?> parent, View view, int position,
+			long id) {
+
+		// 判断是否存在音乐信息
+		if (musicBeans.size() > 0) {
+
+			/*
+			 * Intent intent = new Intent(); intent.putExtra("url",
+			 * getUrl(names.get(position))); intent.putExtra("MSG",
+			 * MusicConstant.PLAY_MSG); intent.setClass(SongSingleActivity.this,
+			 * PlayService.class); startService(intent); //启动服务
+			 */
+			// Toast.makeText(getApplicationContext(),
+			// musicBeans.get(position).getUrl() +"", 0).show();
+			Intent intent2 = new Intent();
+			intent2.putExtra("url", musicBeans.get(position).getUrl());
+			intent2.putExtra("MSG", MusicConstant.PLAY_MSG);
+			intent2.setClass(this, PlayerActivity.class);
+			startActivity(intent2);
+
 		}
-		
+
 	}
-	
-	
-	//读取sdcard中的音乐文件信息
-	public void loadMp3(){
-		
-		//创建一个内容提供对象
+
+	// 读取sdcard中的音乐文件信息
+	public void loadMp3() {
+
+		// 创建一个内容提供对象
 		ContentResolver mResolver = getContentResolver();
-		
-		//设置查询连接地址
+
+		// 设置查询连接地址
 		Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-		
-		//设置查询参数
-		String projetion[] = {MediaStore.Audio.Media._ID,MediaStore.Audio.Media.TITLE,MediaStore.Audio.Media.DATA};
-		
-		//执行查询操作
-		Cursor cursor = mResolver.query(
-                uri, projetion, null, null,null);
-		
-		//判断是否存在查询结果，如果存在循环显示出结果信息
-		if(cursor.getCount() > 0){
-			
+
+		// 设置查询参数
+		String projetion[] = { MediaStore.Audio.Media._ID,
+				MediaStore.Audio.Media.TITLE, MediaStore.Audio.Media.DATA };
+
+		// 执行查询操作
+		// Cursor cursor = mResolver.query(
+		// uri, projetion, null, null,null);
+
+		cursor = mResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+				null, null, null, null);
+
+		// 判断是否存在查询结果，如果存在循环显示出结果信息
+		if (cursor.getCount() > 0) {
+
 			cursor.moveToFirst();
-			while(cursor.moveToNext()){
-				
-				String name = cursor.getString(1);
-				
-				//判断文件后缀
-				if(name.endsWith("mp3")){
-					
-					//去掉文件后缀名
-					name =name.substring(0,name.lastIndexOf("."));
+			while (cursor.moveToNext()) {
+
+				MusicBean musicBean = new MusicBean();
+
+				String name = cursor.getString(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+
+				// 判断文件后缀
+				if (name.endsWith("mp3")) {
+
+					// 去掉文件后缀名
+					name = name.substring(0, name.lastIndexOf("."));
 				}
-				
-				names.add(name);
+				musicBean.setTilte(name);
+
+				musicBean.set_id(cursor.getInt(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)));
+
+				musicBean.setUrl(cursor.getString(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA)));
+
+				musicBean.setAlbum(cursor.getString(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM)));
+
+				musicBean.setArtist(cursor.getString(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)));
+
+				musicBean
+						.setDisplay_name(cursor.getString(cursor
+								.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)));
+
+				musicBean.setYear(cursor.getString(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.YEAR)));
+
+				musicBean
+						.setDuration(cursor.getInt(cursor
+								.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)));
+
+				musicBean.setSize(cursor.getInt(cursor
+						.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)));
+
+				musicBeans.add(musicBean);
 			}
 		}
-		
+
 		/**
-		 * 这里存储根据指定路径查询文件方式，以便之后使用
-		 * File file = new File(PATH);
-		//获取指定路径下的所有文件信息
-		String[] fileNames = file.list();
-		//对数组信息进行迭代找出mp3信息
-		for(String name : fileNames){
-			//判断文件后缀
-			if(name.endsWith("mp3")){
-				
-				//去掉文件后缀名
-				name =name.substring(0,name.lastIndexOf("."));
-				names.add(name);
-			}
-		}*/
+		 * 这里存储根据指定路径查询文件方式，以便之后使用 File file = new File(PATH); //获取指定路径下的所有文件信息
+		 * String[] fileNames = file.list(); //对数组信息进行迭代找出mp3信息 for(String name
+		 * : fileNames){ //判断文件后缀 if(name.endsWith("mp3")){
+		 * 
+		 * //去掉文件后缀名 name =name.substring(0,name.lastIndexOf("."));
+		 * names.add(name); } }
+		 */
 	}
-	
-	//组装音乐路径
-	public String getUrl(String name){
-		
+
+	// 组装音乐路径
+	public String getUrl(String name) {
+
 		return PATH + "/" + name + ".mp3";
 	}
 }
